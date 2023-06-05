8f3db040901e4c14e428f63597dfee7e5c5a2e26
TaliV <tali.vulf@gmail.com>
2023-06-05 11:03:40 +0300

    feat: create basic components and pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
+import { ButtonComponent } from './components/button/button.component';
+import { SearchComponent } from './components/search/search.component';
+import { FavoritesComponent } from './pages/favorites/favorites.component';
+import { HomeComponent } from './pages/home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
+import { TabComponent } from './components/tab/tab.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonComponent,
+    SearchComponent,
+    FavoritesComponent,
+    HomeComponent,
+    HeaderComponent,
+    TabComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot({}, {})
+    StoreModule.forRoot({}, {}),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
