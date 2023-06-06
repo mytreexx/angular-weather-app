43d5cd8697264af9b6d9cee37fd1b844ceb225e5
TaliV <tali.vulf@gmail.com>
2023-06-06 16:30:35 +0300

    feat: get current weather for tel aviv

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -29,6 +30,7 @@ import { MatAutocompleteModule } from '@angular/material/autocomplete';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     StoreModule.forRoot({}, {}),
     BrowserAnimationsModule,
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,31 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { environment } from '../../../environment/enviroment';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  public currentWeather: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getCurrentWeather();
+  }
+
+  public getCurrentWeather() {
+    const params = new HttpParams({}).set('apikey', environment.apiKey);
+
+    this.http
+      .get('http://dataservice.accuweather.com/currentconditions/v1/215854', {
+        params,
+      })
+      .subscribe((data) => {
+        console.log(data);
+        this.currentWeather = data;
+      });
+  }
+}
