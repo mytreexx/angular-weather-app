951932810b01fc34b650ac3b785839d1f1f9ae70
TaliV <tali.vulf@gmail.com>
2023-06-07 15:11:06 +0300

    feat: render daily card component on each forecast day

--- src/app/pages/home/daily-card/daily-card.component.ts
+++ src/app/pages/home/daily-card/daily-card.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-daily-card',
   templateUrl: './daily-card.component.html',
-  styleUrls: ['./daily-card.component.scss']
+  styleUrls: ['./daily-card.component.scss'],
 })
 export class DailyCardComponent {
-
+  @Input() day: any;
 }
