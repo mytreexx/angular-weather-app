52f4cff4b912b85b36c5e02d19d9ec75b84d9c48
TaliV <tali.vulf@gmail.com>
2023-06-07 21:47:11 +0300

    feat: add and remove from favorites functionality in city card

--- src/app/pages/home/city-card/city-card.component.ts
+++ src/app/pages/home/city-card/city-card.component.ts
@@ -1,6 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
 import { CurrentWeather } from 'src/app/services/response';
+import {
+  faHeartCircleMinus,
+  faHeartCirclePlus,
+} from '@fortawesome/free-solid-svg-icons';
+import { FavoritesService } from 'src/app/services/favorites.service';
 
 @Component({
   selector: 'app-city-card',
@@ -10,4 +15,24 @@ import { CurrentWeather } from 'src/app/services/response';
 export class CityCardComponent {
   @Input() weather: CurrentWeather;
   @Input() icon: IconProp;
+
+  constructor(public favorites: FavoritesService) {}
+  cityCode = 215854;
+
+  isFavorite = this.favorites.favorites.some(
+    (favorite) => favorite === this.cityCode
+  );
+  favoriteIcon = this.isFavorite ? faHeartCircleMinus : faHeartCirclePlus;
+
+  toggleFavorite() {
+    this.isFavorite
+      ? this.favorites.removeFromFavorites(this.cityCode)
+      : this.favorites.addToFavorites(this.cityCode);
+    this.isFavorite = this.favorites.favorites.some(
+      (favorite) => favorite === this.cityCode
+    );
+    this.favoriteIcon = this.isFavorite
+      ? faHeartCircleMinus
+      : faHeartCirclePlus;
+  }
 }
